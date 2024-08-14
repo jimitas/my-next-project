f99915ffd483dcf37dd8ba0a62e75ff86c1c056e
jimitas <nonajun805@gmail.com>
2024-08-14 19:18:30 +0900

    8章までのファイル・フォルダコピー

---
+++ app/news/layout.tsx
@@ -0,0 +1,17 @@
+import Hero from "../_components/Hero";
+import Sheet from "../_components/Sheet";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const revalidate = 1;
+
+export default function NewsLayout({ children }: Props) {
+  return (
+    <>
+      <Hero title="News" sub="ニュース" />
+      <Sheet>{children}</Sheet>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,30 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { getNewsList } from "./_libs/microcms";
+import { TOP_NEWS_LIMIT } from "./_constants";
+import NewsList from "./_components/NewsList";
+import ButtonLink from "./_components/ButtonLink";
 
-export default function Home() {
-  return <h1>テクノロジーの力で世界を変える。</h1>;
+export default async function Home() {
+  const data = await getNewsList({ limit: TOP_NEWS_LIMIT });
+
+  return (
+    <>
+      <section className={styles.top}>
+        <div>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>私達は市場をリードしているグローバルカンパニーです。</p>
+        </div>
+        <Image src="/img-mv.jpg" alt="" className={styles.bgimg} width={4000} height={1200} />
+      </section>
+
+      <section className={styles.news}>
+        <h2 className={styles.newsTitle}>News</h2>
+        <NewsList news={data.contents} />
+        <div className={styles.newsLink}>
+          <ButtonLink href="/news">もっとみる</ButtonLink>
+        </div>
+      </section>
+    </>
+  );
 }
